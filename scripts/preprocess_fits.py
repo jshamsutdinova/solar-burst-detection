6dc076ddd02e8d4e93685400201f2da6f7ee91c9
Julia Shamsutdinova <shjn0297@gmail.com>
2025-11-13 20:25:53 +0800

    Added method to create file name

--- scripts/preprocess_fits.py
+++ scripts/preprocess_fits.py
@@ -1,11 +1,8 @@
-import numpy as np
 import cv2
+import numpy as np
+import pandas as pd
 from pathlib import Path
 from astropy.io import fits
-import matplotlib.pyplot as plt
-import scienceplots
-
-plt.style.use(['science', 'notebook', 'grid'])
 
 
 def adaptive_normalization(data):
@@ -27,68 +24,62 @@ def prepare_for_labeling(two_channel_data):
     
     return three_channel_data
 
-def save_as_png(data, save_path):
+def save_as_png(save_path, data):
     """ Save the data as a PNG image. """
     img_data = (data * 255).astype(np.uint8)
+    img_data = cv2.applyColorMap(img_data, cv2.COLORMAP_JET)
     cv2.imwrite(save_path, img_data)
     
+def generate_name(num_file, prefix, file_extension):
+    """ Generate a file name using a pattern with leading zeros. """
+    number = str(num_file).zfill(6)
+    filename = f"{prefix}{number}{file_extension}"
+    return filename
+
+def add_metadata(file, output_csv):
+    """ Write metadata of files into a CSV file."""
+    pass
 
 def prepare_dataset(fits_path):
     with fits.open(fits_path) as hdul:
         data = hdul[0].data
-    
+    print(hdul[0].header)
     h_spec = adaptive_normalization(data[:384, :])
     v_spec = adaptive_normalization(data[384:, :])
     
-    # for training
-    two_channel_data = np.stack([h_spec, v_spec], axis=-1)    
+    two_channel_data = np.stack([h_spec, v_spec], axis=-1) # for training    
+    three_channel_data = prepare_for_labeling(two_channel_data) # for labeling
     
-    # for labeling
-    three_channel_data = prepare_for_labeling(two_channel_data)
-    
-    np.save(f"data/temp/two_channel/two_channel_{fits_path.stem}.npy", two_channel_data)
-    save_as_png(three_channel_data, f"data/temp/three_channel/three_channel_{fits_path.stem}.png")
-    
-    
-    # fig, (ax1, ax2) = plt.subplots(2, 2, figsize=(12, 10))
-    
-    
-    # for ax, method in zip(axes.flat, methods):
-    #     if method == 'percentile_rgb' or method == 'percentile':
-    #         p_low, p_high = np.percentile(data, [5, 95])
-    #         clipped = np.clip(data, p_low, p_high)
-    #         norm_data = (clipped - p_low) / (p_high - p_low)
-    #     elif method == 'adaptive_rgb' or method == 'adaptive':
-    #         median = np.median(data)
-    #         mad = np.median(np.abs(data - median))
-    #         clipped = np.clip(data, median - 2*mad, median + 8*mad)
-    #         norm_data = (clipped - clipped.min()) / (clipped.max() - clipped.min())
-        
-        # img_data = (norm_data * 255).astype(np.uint8)
-        # if method == 'percentile_rgb' or method == 'adaptive_rgb':
-        #     ax.imshow(img_data, cmap='jet', aspect='auto')
-        # elif method == 'percentile' or method == 'adaptive':
-        #     ax.imshow(img_data, cmap='gray', aspect='auto')
-        # ax.set_title(f'Method: {method}')
-        # ax.axis('off')
-    
-    # plt.tight_layout()
-    # plt.savefig(f'data/images/temp/normalization_comparison/{fits_path.stem}.png', dpi=150, bbox_inches='tight')
+    fname_two_ch = generate_name(idx_file, prefix='', file_extension='.npy')
+    fname_three_ch = generate_name(idx_file, prefix='3ch_', file_extension='.png')
 
+    np.save(f"data/temp/two_channel/{fname_two_ch}", two_channel_data)
+    save_as_png(f"data/temp/three_channel/{fname_three_ch}", three_channel_data)
+    
 
 if __name__ == "__main__":
     base_dir = Path("/mnt/d/Work/NSSC/MUSER-L/data/5min_pngfits")
-    dir_to_save = Path("data/images/temp/normalization_comparison")
+    csv_path = Path("data/temp/metadata.csv")
     
+    if csv_path.is_file():
+        df_meta = pd.read_csv(csv_path)
+        print('here')
+    else:
+        columns = ['filename', 'has_burst', 'date', 'start_t' 'fits']
+        df_meta = pd.DataFrame(columns=columns)
+
     dirs = [dir for dir in base_dir.iterdir() if dir.is_dir()]
-    
+    idx_file = 0
     for dir in dirs[:1]:
         files = list(dir.glob("*.fits"))
         print(f"Processing directory: {dir}, found {len(files)} fits files")
 
-        for fits_file in files:
-            prepare_dataset(idx, fits_file)
-            # file_exist = list(dir_to_save.glob(f'{fits_file.stem}*.png'))        
-            # if not file_exist:
-                # compare_normalization_methods(fits_file)
+        for fits_file in files[:1]:
+            if fits_file.stem in df_meta['fits'].values:
+                print(f"File {fits_file} already processed, skipping.")
+            else:
+                prepare_dataset(fits_file)
+                                     
+            idx_file += 1
+            print(f"Proccesed {fits_file}, total files: {idx_file}")
             
